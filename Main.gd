b8a8d27f002fa5270bc67f2f60153549f9a89574
Eggbertx <eggbertx@users.noreply.github.com>
2021-08-03 13:27:50 -0700

    Add level info command

--- Main.gd
+++ Main.gd
@@ -2,21 +2,43 @@ extends Node
 
 var df: DatFile
 
-func load_file(file):
+func load_file(file = ""):
+	if file == "":
+		Console.write_line("File path required")
+		return
+	Console.write_line("Loading %s" % file)
 	df.load_file(file)
-	Console.write_line("Number of levels in %s: %d" % [file, df.num_levels])
 
 func print_info():
-	Console.write_line("Info:")
+	if df.file_path == "":
+		Console.write_line("No dat file loaded")
+		return
+	Console.write_line("Currently loaded dat file: %s" % df.file_path)
+	Console.write_line("Number of levels: %d" % df.num_levels)
+
+func level_info(level = -1):
+	if df.file_path == "":
+		Console.write_line("No dat file loaded")
+		return
+	if level == -1:
+		Console.write_line("No level specified")
+		return
+
+	Console.write_line("Level %d info:" % level)
+	Console.write_line("(todo)")
 
 func register_commands():
 	Console.add_command("loadDAT", self, "load_file")\
 		.set_description("Loads a Chip's Challenge-compatible DAT file")\
-		.add_argument("datfile", TYPE_STRING)\
+		.add_argument("file", TYPE_STRING, "The dat file to be loaded")\
 		.register()
-		
+
+	Console.add_command("levelInfo", self, "level_info")\
+		.add_argument("level", TYPE_INT)\
+		.register()
+
 	Console.add_command("printInfo", self, "print_info")\
-		.set_description("Prints info about the loaded DAT file if one is currently loaded")\
+		.set_description("Prints info about the loaded dat file if one is currently loaded")\
 		.register()
 
 func _input(event):
@@ -29,8 +51,7 @@ func _input(event):
 func _ready():
 	df = DatFile.new()
 	register_commands()
-	# df.load_file("CHIPS.DAT")
-	# print(df.num_levels)
+	load_file("CHIPS.DAT")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
