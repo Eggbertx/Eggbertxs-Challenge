46318b4d4f96eb2baf7f67618273d2f5e830e6d9
Eggbertx <eggbertx@users.noreply.github.com>
2021-11-28 20:39:47 -0800

    Add tileset and basic map mockup

--- src/graphics/LevelMap.gd
+++ src/graphics/LevelMap.gd
@@ -7,13 +7,16 @@ func _ready():
 	# print("tile_set: %s" % $TileMap.tile_set.to_string())
 	tiles_img = Image.new()
 	set_tileset("res://images/old/tileset.png", 32)
-	#$TileMap.tile_set.tile_get_texture()
+	#$Layer1.tile_set.tile_get_texture()
+	#$Layer2.tile_set.tile_get_texture()
 
 func set_tileset(path: String, tile_size: int) -> String:
 	var ts_res = load(path)
 	if ts_res == null:
-		return "Unable to load tileset texture %s" %path
-	$TileMap.tile_set.tile_set_texture(0, load(path))
+		return "Unable to load tileset texture %s" % path
+	return ""
+	# $Layer1.tile_set.tile_set_texture(0, load(path))
+	# $Layer2.tile_set.tile_set_texture(0, load(path))
 #	var tex = AtlasTexture.new()
 	var s = ImageTexture.new()
 	var i = Image.new()
