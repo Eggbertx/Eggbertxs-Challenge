fa3ebeacbbee40755a038f0e41d211305582089b
Eggbertx <eggbertx@users.noreply.github.com>
2023-11-17 16:36:17 -0800

    Temporarily disable custom tileset loading, make tiles draw (somewhat) correctly

--- src/gameplay/LevelMap.gd
+++ src/gameplay/LevelMap.gd
@@ -16,6 +16,8 @@ const DEFAULT_TILESET_SIZE = 32
 const MOVE_DLEAY = 0.3
 
 @onready var tilemap: TileMap = $TileMap
+@onready var camera: Camera2D = $TileMap/Camera2D
+
 var player_pos = Vector2(0, 0)
 var tileset: TileSet
 var tileset_src: TileSetAtlasSource
@@ -34,21 +36,17 @@ var green_keys = 0
 var yellow_keys = 0
 
 func _ready():
-	tileset = TileSet.new()
-	tileset_src = TileSetAtlasSource.new()
-	player_character = MapCharacter.new()
-	for y in range(32):
-		for x in range(32):
-			tilemap.set_cell(0, Vector2i(x, y), Objects.FLOOR)
-
-	var err = set_tileset(DEFAULT_TILESET_PATH, DEFAULT_TILESET_SIZE)
-	if err != "":
-		get_tree().quit()
+	tileset = tilemap.tile_set
+	tileset_src = tileset.get_source(0)
+	player_character = MapCharacter.new(self, camera)
+	camera.position = player_character.position
+	# var err = set_tileset(DEFAULT_TILESET_PATH, DEFAULT_TILESET_SIZE)
+	# if err != "":
+	# 	get_tree().quit()
 
 func _get_atlas(texture: Texture2D, _rect: Rect2) -> AtlasTexture:
 	var atlas = AtlasTexture.new()
 	atlas.set_atlas(texture)
-	# atlas.set_region_enabled(rect)
 	return atlas
 
 func change_game_state(new_state: int):
@@ -60,6 +58,9 @@ func get_game_state() -> int:
 func get_tile(x: int, y: int, layer: int) -> int:
 	return $TileMap.get_cell_source_id(layer - 1, Vector2i(x, y))
 
+func tile_id_to_coords(id: int) -> Vector2i:
+	return tileset_src.get_tile_id(id)
+
 func get_player_tiles():
 	return [
 		$TileMap.get_cell_source_id(0, player_pos),
@@ -67,21 +68,22 @@ func get_player_tiles():
 	]
 
 func set_tile(x: int, y: int, layer: int, tileID: int):
-	$TileMap.set_cell(layer - 1, Vector2i(x, y), tileID)
+	tilemap.set_cell(layer-1, Vector2i(x, y), 0, tile_id_to_coords(tileID))
 
 func change_tile_location(pos1: Vector2i, l1: int, pos2: Vector2i, l2: int):
 	var tile :int = $TileMap.get_cell_source_id(l1-1, pos1)
 	$TileMap.set_cell(l1-1, pos1, -1)
 	$TileMap.set_cell(l2-1, pos2, tile)
 
-func change_character_location(character: MapCharacter, x: int, y: int, layer: int, is_player: bool):
+func change_character_location(character: MapCharacter, x: int, y: int, layer: int):
 	character.position.x = x * 32
 	character.position.y = y * 32
 	character.z_index = layer-1
-	if is_player:
+	if character.player_controlled:
 		player_pos.x = x
 		player_pos.y = y
 		player_layer = layer
+		camera.position = character.position
 
 
 func shift_tile(x: int, y: int, layer: int, direction: String):
@@ -107,44 +109,47 @@ func shift_tile(x: int, y: int, layer: int, direction: String):
 	change_tile_location(Vector2i(x, y), layer, Vector2i(new_x, new_y), layer)
 
 
-func set_tileset(path: String, tile_size: int) -> String:
-	var img:Image
-	if path.begins_with("res://"):
-		tileset_src.texture = load(path)
-		if tileset_src.texture == null:
-			return "Could not load tileset texture %s" % path
-	else:
-		img = Image.new()
-		if img.load(path) != OK:
-			return "Unable to load tileset texture %s" % path
-		tileset_src.texture = ImageTexture.create_from_image(img)
+# TODO: make this actually useful
+# func set_tileset(path: String, tile_size: int) -> String:
+# 	var img:Image
+# 	if path.begins_with("res://"):
+# 		tileset_src.texture = load(path)
+# 		if tileset_src.texture == null:
+# 			return "Could not load tileset texture %s" % path
+# 	else:
+# 		img = Image.new()
+# 		if img.load(path) != OK:
+# 			return "Unable to load tileset texture %s" % path
+# 		tileset_src.texture = ImageTexture.create_from_image(img)
 
-	var img_width = tileset_src.texture.get_width()
-	var img_height = tileset_src.texture.get_height()
-	if img_width % tile_size > 0 or img_height % tile_size > 0:
-		return "Tileset has an invalid size, tile width and height must be multiples of %d" % tile_size
-	tileset_src.texture_region_size = Vector2i(tile_size, tile_size)
+# 	var img_width = tileset_src.texture.get_width()
+# 	var img_height = tileset_src.texture.get_height()
+# 	if img_width % tile_size > 0 or img_height % tile_size > 0:
+# 		return "Tileset has an invalid size, tile width and height must be multiples of %d" % tile_size
+# 	tileset_src.texture_region_size = Vector2i(tile_size, tile_size)
 
-	var x = 0
-	var y = 0
-	var atlases = []
-	for t in range(112):
-		atlases.push_back(_get_atlas(tileset_src.texture, Rect2(x, y, tile_size, tile_size)))
-		# tileset_src.create_tile(Vector2i(x, y), Vector2i(tile_size, tile_size))
+# 	var x = 0
+# 	var y = 0
+# 	var atlases = []
+# 	for t in range(112):
+# 		break
+# 		atlases.push_back(_get_atlas(tileset_src.texture, Rect2(x, y, tile_size, tile_size)))
+# 		# tileset_src.create_tile(Vector2i(x, y), Vector2i(tile_size, tile_size))
 		
-		# tileset.tile_set_texture(t, atlas)
-		if y + tile_size == img_height:
-			y = 0
-			x += tile_size
-		else:
-			y += tile_size
-	$TileMap.tile_set = tileset
+# 		# tileset.tile_set_texture(t, atlas)
+# 		if y + tile_size == img_height:
+# 			y = 0
+# 			x += tile_size
+# 		else:
+# 			y += tile_size
+# 	$TileMap.tile_set = tileset
 
-	player_character.add_sprite_frame("north", atlases[Objects.CHIP_N])
-	player_character.add_sprite_frame("west", atlases[Objects.CHIP_W])
-	player_character.add_sprite_frame("south", atlases[Objects.CHIP_S])
-	player_character.add_sprite_frame("east", atlases[Objects.CHIP_E])
-	return ""
+# 	player_character.add_sprite_frame("north", atlases[Objects.CHIP_N])
+# 	player_character.add_sprite_frame("west", atlases[Objects.CHIP_W])
+# 	player_character.add_sprite_frame("south", atlases[Objects.CHIP_S])
+# 	player_character.add_sprite_frame("east", atlases[Objects.CHIP_E])
+# 	return ""
+
 
 # sets the player position when the map is first loaded, replacing the Objects.CHIP_E tile
 # with a sprite
@@ -154,8 +159,6 @@ func init_player_pos(pos: Vector2i, layer: int, direction: String):
 	player_pos = pos
 	player_layer = layer
 	player_character.player_controlled = true
-	if player_character.parent != null:
-		player_character.parent.remove_child(player_character)
 	$TileMap.add_child(player_character)
 	$TileMap.set_cell(layer-1, player_pos, -1)
 	
@@ -281,7 +284,7 @@ func request_move(direction: String):
 	elif !on_hint and tile_has_hint(new_x, new_y):
 		on_hint = true
 		emit_signal("update_hint_status", true)
-	change_character_location(player_character, new_x, new_y, player_layer, true)
+	change_character_location(player_character, new_x, new_y, player_layer)
 
 func _on_LevelMap_update_chips_left(left: int):
 	chips_left = left
